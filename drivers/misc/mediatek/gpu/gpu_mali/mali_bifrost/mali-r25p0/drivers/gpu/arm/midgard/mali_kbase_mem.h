0ae4eb6dac5d92f6115dfa983f0d44de5d9467ee
Ork <OrkGabb>
2026-03-08 17:10:21 -0300

    Add new limits to mali_kbse pool

--- drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_mem.h
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_mem.h
@@ -677,12 +677,12 @@ static inline int kbase_reg_prepare_native(struct kbase_va_region *reg,
 /*
  * Max size for kbdev memory pool (in pages)
  */
-#define KBASE_MEM_POOL_MAX_SIZE_KBDEV (SZ_64M >> PAGE_SHIFT)
+#define KBASE_MEM_POOL_MAX_SIZE_KBDEV (SZ_256M >> PAGE_SHIFT)
 
 /*
  * Max size for kctx memory pool (in pages)
  */
-#define KBASE_MEM_POOL_MAX_SIZE_KCTX  (SZ_64M >> PAGE_SHIFT)
+#define KBASE_MEM_POOL_MAX_SIZE_KCTX  (SZ_256M >> PAGE_SHIFT)
 
 /*
  * The order required for a 2MB page allocation (2^order * 4KB = 2MB)
