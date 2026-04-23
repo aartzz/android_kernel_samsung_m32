93414899eb482c3f9cb0d9bb8d3339ece03c8ca4
aartzz <migor1103@gmail.com>
2026-04-23 23:03:26 +0300

    dvfs: revert DDR_OPP_0 force, restore safe OPP scaling
    Commit 73cce9b00 forced DDR to maximum speed at all VCORE levels without voltage margin, causing DRAM bit flips proven by ramoops corruption (mangled function names in timer entries). Restore DDR_OPP_1 for VCORE OPP 8-15 so DDR scales down safely with voltage.
    
    Ultraworked with Sisyphus https://github.com/code-yeongyu/oh-my-openagent
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- drivers/devfreq/helio-dvfsrc-v2/helio-dvfsrc-opp-mt6768.c
+++ drivers/devfreq/helio-dvfsrc-v2/helio-dvfsrc-opp-mt6768.c
@@ -166,14 +166,14 @@ void dvfsrc_opp_level_mapping(void)
 		set_ddr_opp(VCORE_DVFS_OPP_5, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_6, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_7, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_12, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_13, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_14, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_15, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
+		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_12, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_13, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_14, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_15, DDR_OPP_1);
 		break;
 	case SPMFW_LP3_1CH_1866:
 		set_vcore_opp(VCORE_DVFS_OPP_0, VCORE_OPP_0);
@@ -201,14 +201,14 @@ void dvfsrc_opp_level_mapping(void)
 		set_ddr_opp(VCORE_DVFS_OPP_5, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_6, DDR_OPP_0);
 		set_ddr_opp(VCORE_DVFS_OPP_7, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_0);
-		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_12, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_13, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_14, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
-		set_ddr_opp(VCORE_DVFS_OPP_15, DDR_OPP_0 /* SYSARCH: DDR_OPP_0 Force */);
+		set_ddr_opp(VCORE_DVFS_OPP_8, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_9, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_10, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_11, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_12, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_13, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_14, DDR_OPP_1);
+		set_ddr_opp(VCORE_DVFS_OPP_15, DDR_OPP_1);
 		break;
 	default:
 		set_vcore_opp(VCORE_DVFS_OPP_0, VCORE_OPP_UNREQ);
