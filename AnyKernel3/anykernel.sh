248b2c547fefe8260f8c344eed5107e070a1ec38
aartzz <migor1103@gmail.com>
2026-07-09 14:21:11 +0300

    anykernel: changed text when flashing kernel

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,11 +1,11 @@
 ### AnyKernel3 Ramdisk Mod Script
-### Modified by @aartzz
+### Modified by @OrkGabb
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=4.14.356-aartzz-fixed-rsuntk
+kernel.string=4.14.356-aartzz
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -42,16 +42,18 @@ PATCH_VBMETA_FLAG=auto;
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
 ui_print " ";
-ui_print "###############################################################";
+ui_print "##############################################";
 ui_print "  Maintainer: aartzz";
-ui_print "  Fork of upstream kernel by RISSU (rsuntk) — many thanks to him.";
+ui_print "  Credits to: rsuntk, OrkGabb";
+ui_print " ";
+ui_print "  Fork of upstream kernel by rsuntk for SM-M325";
 ui_print " ";
 ui_print "  Performance-focused kernel with overclocking, undervolting";
 ui_print "  and selected backports/tweaks for improved responsiveness.";
 ui_print " ";
 ui_print "  DISCLAIMER: Use at your own risk. I do not accept responsibility for";
 ui_print "              any damage that may result from applying this kernel.";
-ui_print "###############################################################";
+ui_print "###############################################";
 ui_print " ";
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
