6bd22ec0546808d7d84dee8560fb5907167b947f
aartzz <migor1103@gmail.com>
2026-07-12 20:54:03 +0300

    AnyKernel3: Bump kernel version to 4.14.357 and update disclaimer

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -5,7 +5,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=4.14.356-aartzz
+kernel.string=4.14.357-aartzz
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -51,8 +51,9 @@ ui_print " ";
 ui_print "  Performance-focused kernel with overclocking, undervolting";
 ui_print "  and selected backports/tweaks for improved responsiveness.";
 ui_print " ";
-ui_print "  DISCLAIMER: Use at your own risk. I do not accept responsibility for";
-ui_print "              any damage that may result from applying this kernel.";
+ui_print "  DISCLAIMER: Use at your own risk. I do not accept";
+ui_print "  responsibility for any damage that may result from";
+ui_print "  applying this kernel.";
 ui_print "###############################################";
 ui_print " ";
 
